db1f6fdc21a7768de6c68ca1941022b839187094
Igor Savin <beixiaofeng@gmail.com>
2018-09-15 19:35:41 +0300

    completed footer

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -3,15 +3,25 @@ import './resources/styles.css';
 
 import Header from './components/header_footer/Header'
 import Featured from './components/featured';
-import VenueNfo from './components/veniueNfo'
+import VenueNfo from './components/venueNfo';
+import Highlight from './components/Highlights';
+import Pricing from './components/pricing';
+import Location from './components/location';
+import Footer from './components/header_footer/Footer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height:"1500px", background:'cornflowerblue'}}>
+      <div className="App" 
+      // style={{height:"1500px", background:'cornflowerblue'}}
+      >
         <Header/>
         <Featured/>
         <VenueNfo/>
+        <Highlight/>
+        <Pricing/>
+        <Location/>
+        <Footer/>
       </div>
     );
   }
